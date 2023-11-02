65439945c2f6878accb0b19b9b26a05f9f444151
iliaz.snowflake@gmail.com <iliaz.snowflake@gmail.com>
2023-11-02 09:03:46 +0000

    customer

--- customer.sql
+++ customer.sql
@@ -23,22 +23,5 @@ customer_orders as (
 
     group by 1
 
-),
-
-final as (
-
-    select
-        customers.customer_id,
-        customers.first_name,
-        customers.last_name,
-        customer_orders.first_order_date,
-        customer_orders.most_recent_order_date,
-        coalesce(customer_orders.number_of_orders, 0) as number_of_orders
-
-    from customers
-
-    left join customer_orders using (customer_id)
-
 )
-
-select * from final
+select * from customer_orders
