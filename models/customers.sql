b7d2faf2c105c99c66d3149747789dedbd91a030
iliaz.snowflake@gmail.com <iliaz.snowflake@gmail.com>
2023-11-02 16:41:38 +0000

    oo

---
+++ models/customers.sql
@@ -0,0 +1,27 @@
+with customers as (
+
+    select * from {{ ref('stg_customers') }}
+
+),
+
+orders as (
+
+    select * from {{ ref('stg_orders') }}
+
+),
+
+customer_orders as (
+
+    select
+        customer_id,
+
+        min(order_date) as first_order_date,
+        max(order_date) as most_recent_order_date,
+        count(order_id) as number_of_orders
+
+    from orders
+
+    group by 1
+
+)
+select * from customer_orders
